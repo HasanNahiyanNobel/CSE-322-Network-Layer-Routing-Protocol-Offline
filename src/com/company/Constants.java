f5e06a4f389a0399870c240061a7d39be30f6c21
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-04-18 12:29:36 +0600

    Initial skeleton.

---
+++ src/com/company/Constants.java
@@ -0,0 +1,9 @@
+package com.company;
+
+//Done!
+public interface Constants {
+
+	final int INFINITY = 10;
+	final double LAMBDA = 0.10;
+
+}
---
+++ src/com/company/RoutingTableEntry.java
@@ -0,0 +1,40 @@
+package com.company;
+
+//Done!
+public class RoutingTableEntry {
+
+	private int routerId;
+	private double distance;
+	private int gatewayRouterId;
+
+	public RoutingTableEntry(int routerId, double distance, int gatewayRouterId) {
+		this.routerId = routerId;
+		this.distance = distance;
+		this.gatewayRouterId = gatewayRouterId;
+	}
+
+	public int getRouterId() {
+		return routerId;
+	}
+
+	public void setRouterId(int routerId) {
+		this.routerId = routerId;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public int getGatewayRouterId() {
+		return gatewayRouterId;
+	}
+
+	public void setGatewayRouterId(int gatewayRouterId) {
+		this.gatewayRouterId = gatewayRouterId;
+	}
+
+}
